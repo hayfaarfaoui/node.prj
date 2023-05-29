1d721a8f7149c15984838e9c7daf99f8fa26e64a
hayfaarfaoui <106407110+hayfaarfaoui@users.noreply.github.com>
2023-05-29 22:52:40 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,20 @@
-const jwt=require("jsonwebtoken");
-const SCREAT_KEY="NodesApi"
-const auth=(req,res,next)=>{
-    try{
-        let token=req.headers.authorization 
-        if(token){
-            token =token.split(" ")[1]
-            let user=jwt.verify(token,SCREAT_KEY)
-            req.userId=user.id
+const jwt = require("jsonwebtoken");
+const SCREAT_KEY = "NodesApi";
+const auth = (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+        if (token) {
+            token = token.split(" ")[1];
+            let user = jwt.verify(token, SCREAT_KEY);
+            req.userId = user.id;
+            next();
+        } else {
+            res.status(401).json({ message: "invalid user" });
         }
-        else{
-            res.status(401).json({message:"invalid user"})
-        }
-        next()
-
-
-    }
-    catch(error){
+    } catch (error) {
+        console.log(error);
         res.status(400).json({ message: "invalid user" });
-        console.log(error)
-
     }
-}
-module.exports=auth;
+};
+
+module.exports = auth;
